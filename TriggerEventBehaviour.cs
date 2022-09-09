9a2ccd2eb7df3a517b105c2ddc7aae9f09daf46b
DJ <swiggettdj@gmail.com>
2022-09-09 01:56:35 -0600

    Added an exit trigger event to triggereventbehaviour

--- TriggerEventBehaviour.cs
+++ TriggerEventBehaviour.cs
@@ -3,10 +3,14 @@ using UnityEngine.Events;
 
 public class TriggerEventBehaviour : MonoBehaviour
 {
-    public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerEnterEvent, triggerExitEvent;
 
     private void OnTriggerEnter(Collider other)
     {
         triggerEnterEvent.Invoke();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
     }
 }
